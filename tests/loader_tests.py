26b9682a4df60796d4c7e0544e253a60e40c6c34
Eren Golge <egolge@mozilla.com>
2018-04-19 07:35:37 -0700

    Adding and correcting tests

--- tests/loader_tests.py
+++ tests/loader_tests.py
@@ -3,9 +3,11 @@ import unittest
 import numpy as np
 
 from torch.utils.data import DataLoader
+from torch.autograd import Variable
 from TTS.utils.generic_utils import load_config
 from TTS.datasets.LJSpeech import LJSpeechDataset
 from TTS.datasets.TWEB import TWEBDataset
+from TTS.datasets.utils import TBPTT
 
 
 file_path = os.path.dirname(os.path.realpath(__file__))
@@ -275,3 +277,67 @@ class TestTWEBDataset(unittest.TestCase):
             # check batch conditions
             assert (mel_input * stop_target.unsqueeze(2)).sum() == 0
             assert (linear_input * stop_target.unsqueeze(2)).sum() == 0
+
+    
+class TestTBPTT(unittest.TestCase):
+
+    def __init__(self, *args, **kwargs):
+        super(TestTBPTT, self).__init__(*args, **kwargs)
+        self.max_loader_iter = 4
+
+    def test_loader(self):
+        dataset = LJSpeechDataset(os.path.join(c.data_path_LJSpeech, 'metadata.csv'),
+                                  os.path.join(c.data_path_LJSpeech, 'wavs'),
+                                  c.r,
+                                  c.sample_rate,
+                                  c.text_cleaner,
+                                  c.num_mels,
+                                  c.min_level_db,
+                                  c.frame_shift_ms,
+                                  c.frame_length_ms,
+                                  c.preemphasis,
+                                  c.ref_level_db,
+                                  c.num_freq,
+                                  c.power
+                                  )
+
+        dataloader = DataLoader(dataset, batch_size=c.batch_size,
+                                shuffle=True, collate_fn=dataset.collate_fn,
+                                drop_last=True, num_workers=c.num_loader_workers)
+        
+        tbp_size = 10
+        for i, data in enumerate(dataloader):
+            if i == self.max_loader_iter:
+                break
+            text_input = data[0]
+            text_lengths = data[1]
+            linear_input = data[2]
+            mel_input = data[3]
+            mel_lengths = data[4]
+            stop_target = data[5]
+            item_idx = data[6]
+            
+            # convert inputs to variables
+            text_input_var = Variable(text_input)
+            mel_spec_var = Variable(mel_input)
+            mel_lengths_var = Variable(mel_lengths)
+            linear_spec_var = Variable(linear_input, volatile=True)
+            
+            tbptt = TBPTT(text_input_var, mel_spec_var, linear_spec_var, mel_lengths_var, tbp_size)
+            
+            for text, mel, linear, lengths in tbptt:
+                if tbptt.start:
+                    assert text.shape[0] == c.batch_size, text.shape
+                    assert mel.shape[0] == c.batch_size
+                    assert mel.shape[1] == tbp_size, mel.shape
+                    assert mel.shape[2] == c.num_mels
+                
+                # check the length consistency
+                assert (mel.shape[1] >= lengths).all()
+                for i, item in enumerate(mel):
+                    length = lengths[i].data[0]
+                    if item.shape[0] > length:
+                        assert item.data[length:].sum() == 0
+                
+
+        
