2816cc2ebd62c34a15efc1a6fcdfe316e2aa2586
Eren <erengolge@gmail.com>
2018-10-05 17:09:25 +0200

    Restore optimiazer and LR scheduler

--- train.py
+++ train.py
@@ -353,6 +353,7 @@ def main(args):
             for k, v in state.items():
                 if torch.is_tensor(v):
                     state[k] = v.cuda()
+        optimizer.load_state_dict(checkpoint['optimizer'])
         print(
             " > Model restored from step %d" % checkpoint['step'], flush=True)
         start_epoch = checkpoint['step'] // len(train_loader)
@@ -366,7 +367,7 @@ def main(args):
             criterion.cuda()
             criterion_st.cuda()
 
-    scheduler = AnnealLR(optimizer, warmup_steps=c.warmup_steps)
+    scheduler = AnnealLR(optimizer, warmup_steps=c.warmup_steps, last_epoch=args.restore_step)
     num_params = count_parameters(model)
     print(" | > Model has {} parameters".format(num_params), flush=True)
 
