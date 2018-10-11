8f9eaa7bb3febc81a71e684589179981794f5ef2
Eren <erengolge@gmail.com>
2018-10-11 12:54:05 +0200

    Random shuffling for preprocessors

--- datasets/preprocess.py
+++ datasets/preprocess.py
@@ -1,12 +1,14 @@
 import os
+import random
 
 def tts_cache(root_path, meta_file):
     txt_file = os.path.join(root_path, meta_file)
     items = []
     with open(txt_file, 'r', encoding='utf8') as f:
         for line in f:
-            cols = line.split('|')
+            cols = line.split('| ')
             items.append(cols)  # wav_full_path, mel_name, linear_name, wav_len, mel_len, text
+    random.shuffle(items)
     return items            
 
 
@@ -39,6 +41,7 @@ def ljspeech(root_path, meta_file):
             wav_file = os.path.join(root_path, 'wavs', cols[0]+'.wav')
             text = cols[1]
             items.append([text, wav_file])
+    random.shuffle(items)
     return items
 
 
@@ -52,4 +55,5 @@ def nancy(root_path, meta_file):
             text = line[line.find('"')+1:line.rfind('"')-1]
             wav_file = root_path + 'wavn/' + id + '.wav'
             items.append(text, wav_file)
+    random.shuffle(items)    
     return items
