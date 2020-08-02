3edecff642d9b80a3c53ea01e26860b8883b560c
jim <jyegerlehner@users.noreply.github.com>
2020-08-01 21:42:47 -0500

    Fix scipy.io.wavfile import

--- utils/audio.py
+++ utils/audio.py
@@ -1,7 +1,7 @@
 import librosa
 import soundfile as sf
 import numpy as np
-import scipy.io
+import scipy.io.wavfile as wavfile
 import scipy.signal
 
 from TTS.utils.data import StandardScaler
@@ -319,7 +319,7 @@ class AudioProcessor(object):
 
     def save_wav(self, wav, path):
         wav_norm = wav * (32767 / max(0.01, np.max(np.abs(wav))))
-        scipy.io.wavfile.write(path, self.sample_rate, wav_norm.astype(np.int16))
+        wavfile.write(path, self.sample_rate, wav_norm.astype(np.int16))
 
     @staticmethod
     def mulaw_encode(wav, qc):
